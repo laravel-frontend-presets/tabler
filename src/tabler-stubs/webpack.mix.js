b7e958fd67f7cfa89778b378c9ee2110743a843a
Daniel Wiser <dwiser@graboyes.com>
2020-05-08 13:24:06 -0400

    extract all tabler vendor packages

--- src/tabler-stubs/webpack.mix.js
+++ src/tabler-stubs/webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
-  .extract(['selectize']);
+  .extract(['apexcharts','autosize','flatpickr','fullcalendar','imask','jqvmap','nouislider','peity','selectize']);
 
 if (mix.inProduction()) {
   mix.version();
